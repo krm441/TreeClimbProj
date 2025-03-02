ebd003f10a33e22c230b88fe8f07988cba92ac6e
OverHive <98308219+OverHive@users.noreply.github.com>
2025-03-02 06:58:10 +0000

    Add files via upload

--- TreeClimbUOL/Assets/Scripts/EnvironmenScripts/GeneralTriggerScripts/MessageTrigger.cs
+++ TreeClimbUOL/Assets/Scripts/EnvironmenScripts/GeneralTriggerScripts/MessageTrigger.cs
@@ -6,12 +6,12 @@ public class MessageTrigger : MonoBehaviour
 {
     static readonly List<string> messages = new List<string>()
     { 
-        "Use the wasd keys or the arrows keys to move.",   
+        "Use the a and keys or the arrows keys to move.",   
         "Press s or down on a platform to fall through it.",
-        "Press spacebar to jump. You can move while jump",
+        "Press spacebar to jump. You can move while jumping",
         "The cars and the mistletoe will harm you on contact. Avoid them!",
         "Press space on a ladder to jump off it.",
-        "Throw snowballs using v. Use them to defeat the snowman!",
+        "Throw snowballs using n. Use them to defeat the snowman!",
         "Throughout the level there are decorations hidden across the level. You can collect them by clicking on them."
     };
 
