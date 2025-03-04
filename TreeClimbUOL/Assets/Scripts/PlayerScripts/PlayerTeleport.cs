9055c73d6306d07a56386410fa1d4a8a08df5e3f
Hesperus <tmw19@student.london.ac.uk>
2025-03-04 19:59:41 +0200

    TestingTpSystem

---
+++ TreeClimbUOL/Assets/Scripts/PlayerScripts/PlayerTeleport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject tpPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log("Entered TP Point: ");
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TPPointEnt")) {
+            tpPoint = other.transform.GetChild(0).gameObject;
+            rb.transform.position = tpPoint.transform.position;
+        }
+    }
+   
+
+
+
+}
